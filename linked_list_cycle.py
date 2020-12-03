88530de18f0b2d11a798ea100760bbf72008cd06
Norris Mei <norrismei3@gmail.com>
2020-12-02 18:02:46 -0800

    Solved challenge. Solution accepted

--- linked_list_cycle.py
+++ linked_list_cycle.py
@@ -6,4 +6,19 @@
 
 class Solution:
     def hasCycle(self, head: ListNode) -> bool:
+        # Create dictionary where node is key, pos is value
+        nodes = {}
+        pos = 0
+        curr = head
         
+        # Traverse the linked list and add nodes and pos into the dictionary
+        while curr is not None:
+            if curr in nodes:
+                return True
+            else:
+                nodes[curr] = [pos]
+                pos += 1
+                curr = curr.next
+        
+        # If made it to the end of the list, then there is no cycle
+        return False
