f77a62ac033c2caf3bcd2fd278bc8adba125db8e
Norris Mei <norrismei3@gmail.com>
2020-12-03 13:15:59 -0800

    Added pseudocode for approach

--- sudoku_finished.py
+++ sudoku_finished.py
@@ -5,6 +5,17 @@ def done_or_not(board): #board[i][j]
   # ..
   # or return 'Try again!'
 
+  # look at each row
+    # look at each col of row
+      # add each num we visit to a set, a matrix of columns, and a matrix of squares
+      # if by the end of the row, the set is less than 9, then there's been a repeat
+  
+  # check the columns in the column matrix to see if any are less than 9
+
+  # check the squares in the square matrix to see if any are less than 9
+
+  # if pass all checks, return "Finished!"
+
 test.assert_equals(done_or_not([[1, 3, 2, 5, 7, 9, 4, 6, 8]
                         ,[4, 9, 8, 2, 6, 1, 3, 7, 5]
                         ,[7, 5, 6, 3, 8, 4, 2, 1, 9]
