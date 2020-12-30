7ba171af610bc2e13baa6ffbd77e7a01b26da5c0
Norris Mei <norrismei3@gmail.com>
2020-12-30 15:17:23 -0800

    Added pseudocode for remove dupes from sorted linked list challenge

--- linked_list_remove_duplicates_from_sorted_ll.py
+++ linked_list_remove_duplicates_from_sorted_ll.py
@@ -9,4 +9,13 @@
 #     SinglyLinkedListNode next
 #
 def removeDuplicates(head):
+    # move through list, comparing current to previous
+        # if current is equal to previous, then this is a duplicate that we have to delete
+            # link previous's next node to current's next node so the linkedlist stays intact
+            # current node updates to the next node we just linked from previous
+            # continue with loop to compare new current to previous
+        # else current is greater than previous, keep moving through linked list
+            # previous becomes the node we just looked at
+            # current node moves on to the next node
     
+    #return head
