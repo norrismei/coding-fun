06f1f09ea73189efc3b7e410c8b0d50d2227f3df
Norris Mei <norrismei3@gmail.com>
2020-12-30 14:18:10 -0800

    Added pseudocode for approach

--- split_string.py
+++ split_string.py
@@ -5,3 +5,19 @@
 # solution('abcdef') # should return ['ab', 'cd', 'ef']
 
 def solution(s):
+    # create list to hold string pairs
+
+    # if string is even, endpoint is end of string
+    # else, endpoint is end of string minus one pair
+
+    # go through loop
+        # keep track of index and index + 1 for the two chars
+        # add the two chars to list of strings
+        # index moves two spaces
+    # if string is odd
+        # make one more pair with last char and underscore
+        # add to list of strings
+    
+    # return list
+
+print(solution('abcd'))
