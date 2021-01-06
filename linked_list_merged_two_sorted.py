e6da9b3cd4387442c1cc22ebfd84cbab20f4c3fc
Norris Mei <norrismei3@gmail.com>
2021-01-06 14:13:27 -0800

    Added pseudocode for merged two sorted llist challenge

--- linked_list_merged_two_sorted.py
+++ linked_list_merged_two_sorted.py
@@ -38,5 +38,24 @@ def print_singly_linked_list(node, sep, fptr):
 #     SinglyLinkedListNode next
 
 def mergeLists(head1, head2):
+# if head1 is null, return head2
+# if head2 is null, return head1
 
+# Create a new linkedlist for our merged linked lists
+# Current node will start at each respective head
+
+# while there are still nodes in list1 and list2, compare the data of the current nodes. 
+    # if node in list1 is less than or equal to node in list2,
+        # insert the node into the merged linked list
+        # curr1 will move to its next node
+    # else, the node in list1 is greater than the node in list2,
+        # so insert the node in list2 to the merged linked list
+        # curr2 will move to its next node
+
+# if curr1 is null, then we've gone through all of list1.
+    # to add the rest of list2, insert curr2 node and move to next while there are still nodes in curr2
+# else, if curr2 is null, then we've gone through all of list2.
+    # to add the rest of list1, insert curr1 node and move to next while there are still nodes in curr1
+
+# return the head of the merged linked list
 
