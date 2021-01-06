e3131b9a65600aa69cfb4880ffcceab5a89310c8
Norris Mei <norrismei3@gmail.com>
2021-01-06 15:15:51 -0800

    Solved who is killer. Could redo using subsets or all() fxn

--- dictionary_who_is_killer.py
+++ dictionary_who_is_killer.py
@@ -13,9 +13,17 @@ def killer(suspect_info, dead):
     # assumptions: suspect is always in the suspect_info. there is only one killer.
 
     # for each victim in the dead list
+    for victim in dead:
         # if the suspects list is down to one person, that person is the killer
+        if len(suspect_info) == 1:
+            return list(suspect_info.keys())[0]
         # for each suspect in the dictionary, see if they saw the victim that day
+        remaining_suspects = list(suspect_info.items())
+        for suspect, people_seen in remaining_suspects:
             # if the suspect did not see victim, suspect can be removed from dictionary altogether
+            if victim not in people_seen:
+                del suspect_info[suspect]
             # else, suspect remains in dictionary and move on to the next suspect
     
     # after going through all victims, there should be one suspect left in dictionary
+    return list(suspect_info.keys())[0]
