8e85e52fe3cf324d1eb7c59adbcdf0b4850efb55
Norris Mei <norrismei3@gmail.com>
2021-02-03 11:05:49 -0800

    Added pseudocode for find nth digit of num

--- find_nth_digit_of_num.py
+++ find_nth_digit_of_num.py
@@ -7,3 +7,10 @@
 # Keep in mind that 42 = 00042. This means that findDigit(42, 5) would return 0
 
 def find_digit(num, nth):
+    # If nth is not positive, return -1
+
+    # If num is negative, multiply by -1 so we can work with pos num
+
+    # Turn num into a string. If the nth is longer than the len of string, return 0.
+
+    # Else, turn nth into a negative index and return the int version of the digit found at index
